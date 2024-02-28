640e9f26b949b3cbee58d2dbe4514509236d35c7
vagrant <vagrant@kali>
2024-02-27 20:09:04 -0500

    shell scripting

---
+++ forLoop.sh
@@ -0,0 +1,7 @@
+# iteration -> for loop
+
+for i in {1.100};
+do echo $1 ;
+done
+
+
---
+++ ifElse.sh
@@ -0,0 +1,13 @@
+a=4
+b=10
+
+if [ $a > $b ]
+then
+	echo 'a  is grater then b'
+
+else 
+	echo 'b is grater then a'
+fi
+:
+
+
